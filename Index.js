ff087862e4fc148e75f689e9f8ec9ee6ab3f95de
Eldhose ER <eldhoseer16@gmail.comgit config --global user.email eldhoseer16@gmail.comgit config --global user.name Eldhose>
2024-03-18 11:45:13 +0530

    Backend

--- Index.js
+++ Index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://password-generator-frontend-sable.vercel.app'],
+    origin: ['https://password-generator-frontend-sable.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 }));
 
