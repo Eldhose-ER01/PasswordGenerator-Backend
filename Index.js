0e4cc45dd9f070939cc04456568ae261893cba37
Eldhose ER <eldhoseer16@gmail.comgit config --global user.email eldhoseer16@gmail.comgit config --global user.name Eldhose>
2024-03-18 11:38:33 +0530

    Backend

--- Index.js
+++ Index.js
@@ -7,9 +7,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.use(cors({
-    origin:'http://localhost:3000',
-    methods:['GET','POST','PUT','PATCH','DELETE']
-}))
+    origin: ['http://localhost:3000', 'https://password-generator-frontend-sable.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+}));
+
 app.use('/',require('./Routes'))
 const PORT = process.env.PORT || 5000; // Use environment variable PORT if available, otherwise default to 5000
 
