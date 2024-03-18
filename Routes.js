cde43466e1cf34cb5904e6083d68e1876bc93cee
Eldhose ER <eldhoseer16@gmail.comgit config --global user.email eldhoseer16@gmail.comgit config --global user.name Eldhose>
2024-03-18 12:48:10 +0530

    Backend

--- Routes.js
+++ Routes.js
@@ -4,5 +4,9 @@ const passwordGenerates=require('./PasswordGen.js')
 
 
 Routes.post('/password',passwordGenerates.passwordGenerate)
+Routes.get('/helo',(req,res)=>{
+res.json({"message":"heooooo"})
+})
+
 
 module.exports=Routes;
